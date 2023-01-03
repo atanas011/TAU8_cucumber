aa8d3b45193d9b5f5bba15509d75849ffe29fab1
atanas011 <atanas011@yahoo.com>
2023-01-03 12:52:23 +0100

    Hooks

--- src/test/java/steps/Steps.java
+++ src/test/java/steps/Steps.java
@@ -1,10 +1,10 @@
 package steps;
 
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import io.cucumber.datatable.DataTable;
-import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -13,23 +13,30 @@ public class Steps {
 
     private WebDriver driver;
 
+    @Before
+    public void setUp() {
+        driver = new EdgeDriver();
+    }
+
     @Given("User is on the Login page of the ParaBank app")
     public void user_is_on_the_login_page_of_the_para_bank_app() {
-        driver = new EdgeDriver();
         driver.get("https://parabank.parasoft.com/parabank/index.htm");
     }
 
-    @When("User enters valid credentials")
-    public void user_enters_valid_credentials(DataTable tbl) {
-        List<List<String>> data = tbl.asLists(String.class);
-        driver.findElement(By.name("username")).sendKeys(data.get(0).get(0));
-        driver.findElement(By.name("password")).sendKeys(data.get(0).get(1));
+    @When("User enters valid {string} and {string}")
+    public void user_enters_valid_credentials(String username, String password) {
+        driver.findElement(By.name("username")).sendKeys(username);
+        driver.findElement(By.name("password")).sendKeys(password);
         driver.findElement(By.name("username")).submit();
     }
 
     @Then("Overview page should be displayed")
     public void user_should_be_taken_to_the_overview_page() {
         driver.findElement(By.linkText("Log Out")).click();
+    }
+
+    @After
+    public void tearDown() {
         driver.quit();
     }
 }
--- src/test/resources/Login.feature
+++ src/test/resources/Login.feature
@@ -3,11 +3,15 @@ Feature: Login Functionality
   As a valid ParaBank client
   User wants to log in successfully
 
-  Scenario: Successful Login
+  # run this
+  Scenario Outline: Successful Login
     Given User is on the Login page of the ParaBank app
-    When User enters valid credentials
-      | Ana | 123 |
+    When User enters valid <username> and <password>
     Then Overview page should be displayed
+    Examples:
+      | username | password |
+      | "Ana"    | "123"    |
+      | "Pera"   | "456"    |
 
 # Ctrl + click on step description > go to step definition file
 # Run > Modify Run Config > Before launch > Build > "-" > "+" > Build Project > OK
